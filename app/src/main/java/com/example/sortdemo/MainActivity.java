8da6e0768480b62fa069bbd3bf46c8c0ea542ec9
chloeho <tho96@csu.fullerton.edu>
2020-09-01 21:39:20 -0700

    updated layout added warningMsg

--- app/src/main/java/com/example/sortdemo/MainActivity.java
+++ app/src/main/java/com/example/sortdemo/MainActivity.java
@@ -9,18 +9,24 @@ import android.view.View;
 
 import android.os.Bundle;
 
+import org.w3c.dom.Text;
+
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private Button sortButton;
     private EditText inputString;
     private TextView sortedString;
+    private TextView warningMsg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sortButton = (Button) findViewById(R.id.sortButton);
         sortedString = (TextView) findViewById(R.id.sortedString);
+        warningMsg = (TextView) findViewById(R.id.warningMsg);
+        
+
         // Add button logic
         sortButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
     public static int[] selectionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             int index = i;
@@ -44,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
             arr[index] = arr[i];
             arr[i] = smallerNumber;
         }
+
         return arr;
     }
     //TODO: Implement sort fucnction
@@ -51,22 +59,36 @@ public class MainActivity extends AppCompatActivity {
         int index = 0;
         String[] numberstr = inputString.split(",");
         int[] numbers = new int[numberstr.length];
+
+        
+        if(numberstr.length < 2){
+            warningMsg.setText("Invalid numbers. Try again!!");
+            return;
+        } else {
+            warningMsg.setText("");
+        }
+
         for(int i = 0; i < numberstr.length; i++){
             try{
-                numbers[index] = Integer.parseInt(numberstr[i].trim());
+                numbers[index] = Integer.parseInt(numberstr[i]);
                 index++;
+        ;
             }
             catch (NumberFormatException nfe)
             {
                 System.out.println(nfe);
             }
         }
+        int count = 0;
         int[] numbersarr = selectionSort(numbers);
         String[] arr = new String[numbersarr.length];
         for(int i=0; i< numbersarr.length; i++){
             arr[i] = String.valueOf(numbersarr[i]);
         }
+
+
         String stuff = Arrays.toString(arr);
         sortedString.setText(stuff);
     }
 }
+
