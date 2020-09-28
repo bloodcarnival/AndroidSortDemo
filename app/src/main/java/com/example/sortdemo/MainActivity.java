d298fcd2e23c143efdf8e7168cef295df6619dc6
Heather Vo <htvo12@uh.edu>
2020-09-27 22:11:57 -0500

    Update MainActivity.java
    Added Bubble Sort. Added textView that showed each line of swapped results. Added beginning array and result array.

--- app/src/main/java/com/example/sortdemo/MainActivity.java
+++ app/src/main/java/com/example/sortdemo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sortdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Html;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -40,20 +42,69 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public static int[] selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            int index = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[index]) {
-                    index = j;
+    /*public static int[] bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
-            }
-            int smallerNumber = arr[index];
-            arr[index] = arr[i];
-            arr[i] = smallerNumber;
+            System.out.println(Arrays.toString(arr));
         }
-
         return arr;
+    }*/
+
+    public void bubbleSort(int[] arr) {
+        sortedString.setText("");
+        int n = arr.length;
+        boolean initial = true;
+        boolean swapped = false;
+        String newArray[] = new String[arr.length];
+        for(int m=0; m<arr.length;m++){
+            newArray[m] = String.valueOf(arr[m]);
+        }
+        for (int i = 0; i < n - 1; i++) {
+            newArray[i] = String.valueOf(arr[i]);
+            for (int j = 0; j < n - i - 1; j++) {
+                if(initial == true) {
+                    sortedString.append(Arrays.toString(arr) + "\n");
+                    initial = false;
+                }
+                //sortedString.append(Html.fromHtml(Arrays.toString(arr) + "\n"));
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    newArray[j] = "<b>" + String.valueOf(arr[j+1]) + "</b>";
+
+                    arr[j + 1] = temp;
+                    newArray[j+1] = "<b>" + String.valueOf(temp) + "</b>";
+                    swapped = true;
+                }
+                if(swapped == true) {
+                    sortedString.append(Html.fromHtml(Arrays.toString(newArray)));
+                    for (int l = 0; l < newArray.length - 1; l++) {
+                        String current = "";
+                        if (newArray[l] != null) {
+                            current = newArray[l];
+                            if (current.contains("<b>")) {
+                                current = current.replace("<b>", "");
+                            } else if (current.contains("<b>")) {
+                                current = current.replace("</b>", "");
+                            }
+                        }
+                        newArray[l] = current;
+                    }
+                    sortedString.append("\n");
+                }
+                swapped = false;
+            }
+        }
+        sortedString.append("Results: \n");
+        sortedString.append(Arrays.toString(arr) + "\n");
     }
     //TODO: Implement sort function
     private void sort(String inputString) {
@@ -61,38 +112,38 @@ public class MainActivity extends AppCompatActivity {
         int index = 0;
         String[] numberstr = inputString.split(",");
         int[] numbers = new int[numberstr.length];
-
+        boolean warning = false;
         
-        if(numberstr.length < 2){
+        if(numberstr.length < 3 || numberstr.length > 9){
             warningMsg.setText("Invalid numbers. Try again!!");
+            warning = true;
             return;
         } else {
             warningMsg.setText("");
+            warning = false;
         }
-
-        for(int i = 0; i < numberstr.length; i++){
-            try{
-                numbers[index] = Integer.parseInt(numberstr[i]);
-                index++;
-        ;
+        if(warning == false) {
+            for (int i = 0; i < numberstr.length; i++) {
+                try {
+                    numbers[index] = Integer.parseInt(numberstr[i]);
+                    index++;
+                } catch (NumberFormatException nfe) {
+                    System.out.println(nfe);
+                }
             }
-            catch (NumberFormatException nfe)
-            {
-                System.out.println(nfe);
-            }
-        }
-        int count = 0;
-        int[] numbersarr = selectionSort(numbers);
-        String[] arr = new String[numbersarr.length];
+            //int[] numbersarr = bubbleSort(numbers);
+        /*String[] arr = new String[numbersarr.length];
         for(int i=0; i< numbersarr.length; i++){
             arr[i] = String.valueOf(numbersarr[i]);
+        }*/
+
+
+            //String stuff = Arrays.toString(numbersarr);
+            //sortedString.setText(stuff);
+            bubbleSort(numbers);
+            long endTime = System.currentTimeMillis();
+            duration.setText(endTime + " milliseconds");
         }
-
-
-        String stuff = Arrays.toString(arr);
-        sortedString.setText(stuff);
-        long endTime = System.currentTimeMillis();
-        duration.setText(endTime + " milliseconds");
     }
 }
 
